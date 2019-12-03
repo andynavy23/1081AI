2e0e2d20c717f78c2abf183a091a80e1a7e6e475
andynavy23 <andynavy23@gmail.com>
2019-12-03 20:10:42 +0800

    Update AIp12e.py

--- AIp12e.py
+++ AIp12e.py
@@ -48,10 +48,12 @@ def handle_message(event):
     elif(text=="機器人"):
         reply_text = "有！我是機器人，在！"
     elif(text=="讀取資料"):
+        """
         with open('test.csv', newline='') as csvfile:
             rows = csv.reader(csvfile)
             content = " ".join(rows)
-        reply_text = 'dd'
+        """
+        reply_text = "dd"
     else:  # 如果非以上的選項，就會學你說話
         reply_text = text
     message = TextSendMessage(reply_text)
